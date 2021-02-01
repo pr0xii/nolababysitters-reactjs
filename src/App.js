143400a58b057d23c665f6f76989223095a27f90
pr0xii <55070729+pr0xii@users.noreply.github.com>
2021-01-31 20:21:28 -0600

    .
    Things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './global';
-import { theme } from './theme';
-import { Burger, Menu } from './components';
-import Typed from 'react-typed';
-import { useOnClickOutside } from './hooks';
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { Burger, Menu } from "./components";
+import Typed from "react-typed";
+import { useOnClickOutside } from "./hooks";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -17,13 +17,17 @@ function App() {
         <div class="type-wrap">
           <span id="typed">
             <Typed
-                strings={['Welcome to NOLA Babysitters.', 'We are here to help you find the perfect sitter for your needs!']}
-                typeSpeed={30}
-                backSpeed={50}
-            loop/>
+              strings={[
+                "Welcome to NOLA Babysitters.",
+                "We are here to help you find the perfect sitter for your needs!",
+              ]}
+              typeSpeed={30}
+              backSpeed={50}
+              loop
+            />
           </span>
-            
-          <br/>
+
+          <br />
         </div>
         <div ref={node}>
           <Burger open={open} setOpen={setOpen} />
