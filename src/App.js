d5569edeb53eb8efdb736d3a41374e1db4312b8f
pr0xii <55070729+pr0xii@users.noreply.github.com>
2021-01-31 19:41:16 -0600

    Close menu by clicking outside of menu

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./global";
-import { theme } from "./theme";
-import { Burger, Menu } from "./components";
-import Typed from "react-typed";
+import React, { useState, useRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import { Burger, Menu } from './components';
+import Typed from 'react-typed';
+import { useOnClickOutside } from './hooks';
 
 function App() {
   const [open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
-        <div>
-          <Typed
-            strings={[
-              "Welcome to NOLA Babysitters.",
-              "Do you need to find the perfect sitter?",
-              "We'd love to help..",
-              "And we hope you enjoy your stay!",
-            ]}
-            typeSpeed={40}
-            backSpeed={30}
-            loop
-          />
-          <br />
+        <div class="type-wrap">
+          <span id="typed">
+            <Typed
+                strings={['Welcome to NOLA Babysitters.', 'We are here to help you find the perfect sitter for your needs!']}
+                typeSpeed={30}
+                backSpeed={50}
+            loop/>
+          </span>
+            
+          <br/>
         </div>
-        <div>
+        <div ref={node}>
           <Burger open={open} setOpen={setOpen} />
           <Menu open={open} setOpen={setOpen} />
         </div>
