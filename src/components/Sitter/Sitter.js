7022907c20584f5e4c355392af9681316f1e149f
pr0xii <55070729+pr0xii@users.noreply.github.com>
2021-02-03 04:06:52 -0600

    Add Carousel

--- src/components/Sitter/Sitter.js
+++ src/components/Sitter/Sitter.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 
-export default function Sitter(){
-    return(
-        <div>
-          <p>Here you will pick a sitter.</p>
-        </div>
+class Sitter extends React.Component {
+  renderThumbs = () =>
+    <ul>
+      {
+        [1,2,3,4,5].map((item, i) =>
+          <li key={i} onClick={() => this.Carousel._onDotClick(i)}>Thumb {item}</li>)
+      }
+    </ul>;
+
+  render() {
+    return (
+      <div>
+        <h3>Navigation</h3>
+        { this.renderThumbs() }
+        <button onClick={() => this.Carousel._slidePrev()}>Prev button</button>
+        <button onClick={() => this.Carousel._slideNext()}>Next button</button>
+        <h3>React Alice Carousel</h3>
+        <AliceCarousel
+          dotsDisabled={true}
+          buttonsDisabled={true}
+          ref={ el => this.Carousel = el }
+        >
+          <div className="yours-custom-class"><h2>Test One</h2></div>
+          <div className="yours-custom-class"><h2>Test Two</h2></div>
+          <div className="yours-custom-class"><h2>Test Three</h2></div>
+          <div className="yours-custom-class"><h2>Test Four</h2></div>
+          <div className="yours-custom-class"><h2>Test Five</h2></div>
+        </AliceCarousel>
+      </div>
     );
+  }
 }
+
+export default Sitter;
